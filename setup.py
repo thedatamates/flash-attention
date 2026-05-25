0546e68b59d870b1dca2291b81cfc9df49704484
Josh Ferguson <josh@thedatamates.com>
2026-05-25 08:56:47 -0500

    Package flash_attn.cute modules

--- setup.py
+++ setup.py
@@ -694,8 +694,6 @@ setup(
             "docs",
             "benchmarks",
             "flash_attn.egg-info",
-            "flash_attn.cute",
-            "flash_attn.cute.*",
         )
     ),
     author="Tri Dao",
